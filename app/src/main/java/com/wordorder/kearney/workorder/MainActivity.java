692d2dc20cf9b7699b8174c16c758dc5abbfac52
David <david.kearney@cfl.rr.com>
2017-11-01 16:51:04 -0400

    Second Commit

--- app/src/main/java/com/wordorder/kearney/workorder/MainActivity.java
+++ app/src/main/java/com/wordorder/kearney/workorder/MainActivity.java
@@ -1,132 +1,33 @@
 package com.wordorder.kearney.workorder;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView clientNameLabel;
-    private EditText clientNameText;
-    private TextView projectNameLabel;
-    private EditText projectNameText;
-    private TextView changeOrderLabel;
-    private EditText changeOrderText;
-    private TextView requestedByLabel;
-    private EditText requestedByText;
-    private TextView subcontractorNameLabel;
-    private EditText subcontractorNameText;
-    private TextView dateRequestedLabel;
-    private EditText dateRequestedText;
-    private TextView dateScheduledLabel;
-    private EditText dateScheduledText;
-
-    private Button nextBtn;
-    private Button backBtn;
-    private Button submitBtn;
-    private String subject;
-    private String body;
-    private String emailSend;
-    private String emailReceive;
-    private String password;
+    private Button newWorkOrderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initialize();
         start();
     }
 
-    private void initialize(){
-        clientNameLabel        = (TextView) findViewById(R.id.clientNameLabel);
-        clientNameText         = (EditText) findViewById(R.id.clientNameText);
-        projectNameLabel       = (TextView) findViewById(R.id.projectNameLabel);
-        projectNameText        = (EditText) findViewById(R.id.projectNameText);
-        changeOrderLabel       = (TextView) findViewById(R.id.changeOrderLabel);
-        changeOrderText        = (EditText) findViewById(R.id.changeOrderText);
-        requestedByLabel       = (TextView) findViewById(R.id.requestedByLabel);
-        requestedByText        = (EditText) findViewById(R.id.requestedByText);
-        subcontractorNameLabel = (TextView) findViewById(R.id.subcontractorNameLabel);
-        subcontractorNameText  = (EditText) findViewById(R.id.subcontractorNameText);
-        dateRequestedLabel     = (TextView) findViewById(R.id.dateRequestedLabel);
-        dateRequestedText      = (EditText) findViewById(R.id.dateRequestedText);
-        dateScheduledLabel = (TextView) findViewById(R.id.dateScheduledLabel);
-        dateScheduledText      = (EditText) findViewById(R.id.dateScheduledText);
-
-        nextBtn = (Button) findViewById(R.id.nextButton);
-        submitBtn = (Button) findViewById(R.id.submitBtn);
-    }
-
     private void start(){
+        newWorkOrderBtn = (Button) findViewById(R.id.workOrdersBtn);
 
-        nextBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                setContentView(R.layout.activity_main_2);
-
-                backBtn = (Button )findViewById(R.id.backBtn);
-                backBtn.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v){
-                        setContentView(R.layout.activity_main);
-                    }
-                });
-            }
-        });
-
-
-        /*
-        submitBtn.setOnClickListener(new View.OnClickListener() {
+        newWorkOrderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // Email Subject
-                subject = "Work Order";
-
-                // Email Body
-                body = clientNameLabel.getText().toString() + ": ";
-                body += clientNameText.getText().toString() + System.getProperty("line.separator");
-                body += projectNameLabel.getText().toString() + ": ";
-                body += projectNameText.getText().toString() + System.getProperty("line.separator");
-                body += changeOrderLabel.getText().toString() + ": ";
-                body += changeOrderText.getText().toString() + System.getProperty("line.separator");
-                body += requestedByLabel.getText().toString() + ": ";
-                body += requestedByText.getText().toString() + System.getProperty("line.separator");
-                body += subcontractorNameLabel.getText().toString() + ": ";
-                body += subcontractorNameText.getText().toString() + System.getProperty("line.separator");
-                body += dateRequestedLabel.getText().toString() + ": ";
-                body += dateRequestedText.getText().toString() + System.getProperty("line.separator");
-                body += dateScheduledLabel.getText().toString() + ": ";
-                body += dateScheduledText.getText().toString() + System.getProperty("line.separator");
-
-                // Email Recipient
-                emailSend = "jbwa.workorder@gmail.com";
-
-                // Email Sender
-                emailReceive = "david.kearney@webbengr.com";
-                password = "jbwaworkorder";
-
-                // Debugging Purposes
-                Log.d("Subject: ", subject);
-                Log.d("Body: ", body);
-                Log.d("Email Send: ", emailSend);
-                Log.d("Email Receive: ", emailReceive);
-                Log.d("Password: ", password);
-
-                try {
-                    GMailSender sender = new GMailSender(emailSend, password);
-                    sender.sendMail(subject, body, emailSend, emailReceive);
-                } catch (Exception e){
-                    Log.e("SendMail External", e.getMessage(), e);
-                }
+                Intent intent = new Intent(v.getContext(), WorkOrder.class);
+                startActivity(intent);
             }
         });
-        */
     }
 
     @Override
---
+++ app/src/main/java/com/wordorder/kearney/workorder/WorkOrder.java
@@ -0,0 +1,134 @@
+package com.wordorder.kearney.workorder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by David on 11/1/2017.
+ */
+
+public class WorkOrder extends AppCompatActivity {
+
+    private TextView clientNameLabel;
+    private EditText clientNameText;
+    private TextView projectNameLabel;
+    private EditText projectNameText;
+    private TextView changeOrderLabel;
+    private EditText changeOrderText;
+    private TextView requestedByLabel;
+    private EditText requestedByText;
+    private TextView subcontractorNameLabel;
+    private EditText subcontractorNameText;
+    private TextView dateRequestedLabel;
+    private EditText dateRequestedText;
+    private TextView dateScheduledLabel;
+    private EditText dateScheduledText;
+
+    private Button nextBtn;
+    private String subject;
+    private String body;
+    private String emailSend;
+    private String emailReceive;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_order);
+
+        Intent intent = getIntent();
+
+        initialize();
+        start();
+    }
+
+    private void initialize(){
+        clientNameLabel        = (TextView) findViewById(R.id.clientNameLabel);
+        clientNameText         = (EditText) findViewById(R.id.clientNameText);
+        projectNameLabel       = (TextView) findViewById(R.id.projectNameLabel);
+        projectNameText        = (EditText) findViewById(R.id.projectNameText);
+        changeOrderLabel       = (TextView) findViewById(R.id.changeOrderLabel);
+        changeOrderText        = (EditText) findViewById(R.id.changeOrderText);
+        requestedByLabel       = (TextView) findViewById(R.id.requestedByLabel);
+        requestedByText        = (EditText) findViewById(R.id.requestedByText);
+        subcontractorNameLabel = (TextView) findViewById(R.id.subcontractorNameLabel);
+        subcontractorNameText  = (EditText) findViewById(R.id.subcontractorNameText);
+        dateRequestedLabel     = (TextView) findViewById(R.id.dateRequestedLabel);
+        dateRequestedText      = (EditText) findViewById(R.id.dateRequestedText);
+        dateScheduledLabel = (TextView) findViewById(R.id.dateScheduledLabel);
+        dateScheduledText      = (EditText) findViewById(R.id.dateScheduledText);
+
+        nextBtn = (Button) findViewById(R.id.nextButton);
+    }
+
+    private void start(){
+
+        nextBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                // Email Subject
+                subject = "Work Order";
+
+                // Email Body
+                body = clientNameLabel.getText().toString() + ": ";
+                body += clientNameText.getText().toString() + System.getProperty("line.separator");
+                body += projectNameLabel.getText().toString() + ": ";
+                body += projectNameText.getText().toString() + System.getProperty("line.separator");
+                body += changeOrderLabel.getText().toString() + ": ";
+                body += changeOrderText.getText().toString() + System.getProperty("line.separator");
+                body += requestedByLabel.getText().toString() + ": ";
+                body += requestedByText.getText().toString() + System.getProperty("line.separator");
+                body += subcontractorNameLabel.getText().toString() + ": ";
+                body += subcontractorNameText.getText().toString() + System.getProperty("line.separator");
+                body += dateRequestedLabel.getText().toString() + ": ";
+                body += dateRequestedText.getText().toString() + System.getProperty("line.separator");
+                body += dateScheduledLabel.getText().toString() + ": ";
+                body += dateScheduledText.getText().toString() + System.getProperty("line.separator");
+
+
+
+                // Email Recipient
+                emailSend = "jbwa.workorder@gmail.com";
+
+                // Email Sender
+                emailReceive = "david.kearney@webbengr.com";
+                password = "jbwaworkorder";
+
+                // Debugging Purposes
+                Log.d("Subject: ", subject);
+                Log.d("Body: ", body);
+                Log.d("Email Send: ", emailSend);
+                Log.d("Email Receive: ", emailReceive);
+                Log.d("Password: ", password);
+
+                try {
+                    GMailSender sender = new GMailSender(emailSend, password);
+                    sender.sendMail(subject, body, emailSend, emailReceive);
+                } catch (Exception e){
+                    Log.e("SendMail External", e.getMessage(), e);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
